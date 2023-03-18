bcdd0cd72748492490df3366245ff4e1a30be1c1
naehyun25 <yoon658@naver.com>
2023-03-18 16:44:30 +0900

    서버 파일처리 및 상품생성조회

--- index.js
+++ index.js
@@ -1,76 +1,143 @@
-//express, cors 모듈 import
+//import React from "react";와 같은개념
 const express = require("express");
 const cors = require("cors");
+const app = express();
+const port = 8080;//http://127.0.0.1:8080/
+const models = require("./models");/* ./models/index.js */
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },//경로설정
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
 
-const app=express();//주인공
-const port = 8080;
-
-const models=require("./models");
-
-//json 형식의 데이터를 처리할 수 있게 설정(아래 두줄은 환경설정한거임)
+//json 형식의 데이터를 처리할수 있게 설정
 app.use(express.json());
-app.use(cors());//cors를 app이 쓰겠다.
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
 
-//경로설정부분
-    //상품조회
-app.get('/products',(req,res) => {
-    //findAll 함수
-    models.Product.findAll()
+//rest API
+//method,경로설정(요청,응답)
+app.get("/products", (req, res) => {
+  models.Product.findAll({
+    order: [["createdAt", "ASC"]],//정렬기능
+    attributes: ["id", "name", "price", "seller", "imageUrl", "createdAt"],
+    //limit:1,//제한을 거는거(1개만 나옴)
+    //조회 결과값을 조정할 수 있다. 로딩속도 개선에 좋음
+    //'참조컬럼','ASC'(오름차순) || 'DESC' (내림차순) =>안쓰면 id기준 오름차순
+  })
+  .then((result) => {//모든 데이터는 비동기식
+    console.log("product 조회결과:", result);
+    res.send({ products: result });
+  })
+  .catch((err) => {
+    console.error(err);
+    res.send("에러발생");
+  });
+});
+  
+app.get("/products/:id", (req, res) => {
+  const params = req.params;
+  //const id=params.id;
+  //const { id, eventId } = params;
+  const { id } = params;
+  models.Product.findOne({
+    where: { id: id },
+  })
     .then((result) => {
-        console.log("product조회결과:",result);
-        res.send({product:result});
-    })
-    .catch((err) => {
-        console.error(err);
-        res.send("에러발생");
-    }) 
-})
-    //상품등록(업로드)=>데이터베이스추가
-app.post('/products',(req,res) => {
-    const body = req.body
-    //1. 상수 body에 전달받은 값을 구조분해 할당
-    const {name, price,seller,description, imageUrl}=body;
-            //추가=> 방어코드 
-            //(아니면 뒤로가기 누르면 이전에 작성했던게 남아있게하거나)
-    if(!name || !description || !price || !seller){
-        res.send("모든 필드를 입력해주세요")
-      }
-
-
-    //2. 레코드생성 : 레코드는 행단위의 데이터(기준은 열이었음위에거)
-    //models 는 우리가 임포트해서 만든 상수(맨위에)
-    models.Product.create({name, price,seller,description, imageUrl})
-    //3.
-    .then((result) => {
-        console.log("상품생성결과:",result);
-        res.send({result})
+      console.log("조회결과 :", result);
+      res.send({
+        product:result
+      })
     })
     .catch((error) => {
-        console.error(error)
-        res.send("상품업로드에 문제가 발생했습니다.")
-    });
-    
-})
-app.get('/products/:id/events/:eventId',(req,res) => {
-    const params = req.params;
-    //const id = params.id;
-    const {id,eventId}=params;
-        res.send(`id는 ${id}입니다 evenId는 ${eventId}`)
-})
-//method:post, /login 로그인이 완료되었습니다.
-app.post('/login',(req,res)=>{
-    res.send("로그인이 완료되었습니다.")
+      console.log("에러 :", error);
+    }); 
+});
+//get은 데이터를 못보냄 받아뿌리기만함
+
+app.post('/image', upload.single('image'),(req,res) => {
+    const file = req.file;
+    console.log(file);
+    res.send({
+        imageUrl:file.path,
+    })
 })
 
-//app 실행 부분(이 위는 경로 설정)
-app.listen(port,() => {
-    console.log("망고샵의 쇼핑몰 서버가 돌아가고 있습니다.왈왈")
-    models.sequelize
+//상품생성데이터를 데이터베이스에 추가
+app.post("/products", (req, res) => {
+  const body = req.body;
+  //submit으로 보낸 데이터를 req(요청).body로 받고 그걸 상수 body에 담음
+  //1. 디스트럭처링으로 상수 body 의 값을 개별적으로 할당한다
+  const { name, description, price, seller } = body;
+  if (!name || !description || !price || !seller) {
+    res.send("모든 필드를 입력해주세요");
+  }
+  //2. 레코드 생성 (행단위): Product테이블에 괄호안의 객체를 생성함
+  models.Product.create({ 
+    name, 
+    description, 
+    price, 
+    seller
+  })
+   //3. 데이터를 다루는 것은 기본적으로 비동기 통신을 지원하므로 promise 객체 활용
+   .then((result) => {
+    console.log("상품생성결과:", result);
+    res.send({ result });
+  })
+  .catch((error) => {
+    console.error(error);
+    res.send("상품 업로드에 문제가 발생했습니다.");
+  });
+//res.send({ body });
+});
+app.post("/login", (req, res) => {
+  res.send("로그인이 완료 되었습니다.");
+});
+
+//app 실행
+app.listen(port, () => {
+  console.log("망고샵의 쇼핑몰 서버가 돌아가고 있습니다. 으르렁왈왈왈🐶🐶");
+  models.sequelize
     .sync()
-    .then(() => {console.log('🎆db연결성공');})
+    .then(() => {
+      console.log("😀db연결 성공");
+    })
     .catch((err) => {
-        console.error(err); 
-        console.log('🔊db연결에러'); 
-        process.exit(/* 에러시 서버종료 하겠다는 명령어 */);});
-    //sequelize.sync(->접속하겠다) :sequelize 초기화시 연결되면 테이블을 생성해준다.
-})
+      console.err(err);
+      console.log("😫db연결 실패");
+      process.exit();
+    });
+
+  //method: post, /login 로그인이 완료되었습니다
+app.post("/login", (req, res) => {
+  res.send("로그인이 완료되었습니다");
+});
+
+  /* sync() 접속 
+    sequelize.sync() db에 필요한 테이블 생성
+    process.exit() 서버종료하는 명령어
+   */
+});
+
+/* sqlite =>mysql을 쉽게 다루게 경량화함
+    ORM => sqlite를 더 쉽게 사용함
+*/
+
+/* 
+  npx sequelize init 을 하면 아래 폴더들이 생성 된다.
+├── ...
+├── config  //sequelize와 연결될 데이터베이스 설정
+│   └── config.json 
+├── models  //데이터베이스 모델링 관련 설정
+│   └── index.js
+├── seeders
+└── migrations
+
+*/
+
